9fe1757f023a7781515cf89fb84a3ae23cd36270
freddy.liu <freddy.liu@positivegrid.com>
2023-09-26 23:11:31 +0800

    feat: add prisma lib

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import { Elysia } from 'elysia';
-
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
-
+import Elysia from 'elysia';
+import { auth } from '@/modules/auth';
+import { cookie } from '@elysiajs/cookie';
+import { jwt } from '@elysiajs/jwt';
+const app = new Elysia()
+  .group('/api', (app) =>
+    app
+      .use(
+        jwt({
+          name: 'jwt',
+          secret: Bun.env.JWT_SECRET || '',
+        }),
+      )
+      .use(cookie())
+      .use(auth),
+  )
+  .listen(8080);
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
